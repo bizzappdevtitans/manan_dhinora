35206570f9971c22beb5a8e2f4465c274fba6893
bizzappdev <BAD@bizzappdev.com>
2023-10-12 19:19:29 +0530

    [WIP][T00464]add_attachments: made a new module

---
+++ add_attachments/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Add attachments",
+    "website": "https://github.com/OCA/contract",
+    "author": "BizzAppDev",
+    "category": "Sales/Sales",
+    "version": "15.0.0.0.1",
+    "license": "LGPL-3",
+    "depends": ["base", "sale_management"],
+    "data": [
+        "views/email_compose_message_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ add_attachments/models/email_compose_message.py
@@ -0,0 +1,52 @@
+from odoo import models
+
+
+class MailComposeMessage(models.TransientModel):
+    _inherit = "mail.compose.message"
+
+    def attachments(self):
+        """using a button to fetch all the required attachments to then add then
+        to attachment_ids #T00464"""
+        sale_order = self.env.context.get("active_id")
+        attachment = self.env["ir.attachment"]
+        products = self.env["sale.order"].browse([sale_order]).order_line.product_id
+        template_products = []
+        for product_temp in products:
+            template_ids = (
+                self.env["product.product"].browse([product_temp]).product_tmpl_id
+            )
+            template_products.append(template_ids.id)
+        # finding product attachments in product.product
+        if len(products.ids) > 0:
+            product_attachment = []
+            for prod_product in products.ids:
+                product_product_attachment = attachment.search(
+                    [
+                        ("res_model", "=", "product.product"),
+                        ("res_id", "=", prod_product),
+                    ]
+                )
+                product_attachment.append(product_product_attachment)
+        # finding product attachments in product.template
+        if len(template_products) > 0:
+            template_attachment = []
+            for tmp_product in template_products:
+                product_template_attachment = attachment.search(
+                    [
+                        ("res_model", "=", "product.template"),
+                        ("res_id", "=", tmp_product),
+                    ]
+                )
+                template_attachment.append(product_template_attachment.id)
+        # finding attachments in sale.order
+        sale_attachment = attachment.search(
+            [
+                ("res_model", "=", "sale.order"),
+                ("res_id", "=", sale_order),
+            ]
+        )
+        # making a list of all the product that are to be attached in the email
+        net_attachment = (
+            sale_attachment + product_product_attachment + product_template_attachment
+        )
+        self.write({"attachment_ids": net_attachment})
