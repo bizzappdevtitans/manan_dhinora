7f1fbcbcabc3906f85892d5dc1c3c93f473e1230
bizzappdev <BAD@bizzappdev.com>
2023-10-13 14:05:51 +0530

    Revert "[WIP][T00464]add_attachments: updated a method"
    This reverts commit 5964c4f1dff5cf3df064c9830592637346aa9cbf.

--- add_attachments/models/email_compose_message.py
+++ add_attachments/models/email_compose_message.py
@@ -11,7 +11,7 @@ class MailComposeMessage(models.TransientModel):
         attachment = self.env["ir.attachment"]
         products = self.env["sale.order"].browse([sale_order]).order_line.product_id
         template_products = []
-        for product_temp in products.ids:
+        for product_temp in products:
             template_ids = (
                 self.env["product.product"].browse([product_temp]).product_tmpl_id
             )
@@ -26,20 +26,18 @@ class MailComposeMessage(models.TransientModel):
                         ("res_id", "=", prod_product),
                     ]
                 )
-                for prod_product_attachment in product_product_attachment.ids:
-                    product_attachment.append(prod_product_attachment)
+                product_attachment.append(product_product_attachment)
         # finding product attachments in product.template
         if len(template_products) > 0:
             template_attachment = []
             for tmp_product in template_products:
-                product_template_attachments = attachment.search(
+                product_template_attachment = attachment.search(
                     [
                         ("res_model", "=", "product.template"),
                         ("res_id", "=", tmp_product),
                     ]
                 )
-                for tmp_product_attachment in product_template_attachments.ids:
-                    template_attachment.append(tmp_product_attachment)
+                template_attachment.append(product_template_attachment.id)
         # finding attachments in sale.order
         sale_attachment = attachment.search(
             [
@@ -47,8 +45,8 @@ class MailComposeMessage(models.TransientModel):
                 ("res_id", "=", sale_order),
             ]
         )
-        # making a final list of all the product that are to be attached in the email
-        net_attachment = sale_attachment.ids + product_attachment + template_attachment
-
-        for i in net_attachment:
-            self.write({"attachment_ids": [(4, i)]})
+        # making a list of all the product that are to be attached in the email
+        net_attachment = (
+            sale_attachment + product_product_attachment + product_template_attachment
+        )
+        self.write({"attachment_ids": net_attachment})
